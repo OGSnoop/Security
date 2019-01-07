88470da076fb9f914f35e6413dea88d956d14aae
OGSnoop <43729699+OGSnoop@users.noreply.github.com>
2019-01-07 15:52:55 -0600

    Add files via upload

---
+++ light_install.sh
@@ -0,0 +1,14 @@
+!#/bin/bash
+apt update
+apt install python3.7 idle-python3.7 gnome-screenshot python3-pip gedit net-tools
+pip3 install pycryptodome
+touch /root/Documents/myfile.txt /root/Documents/file.txt /root/Documents/newfile.txt
+echo fishcakes >> myfile.txt
+echo fish >> myfile.txt
+echo cakes >> myfile.txt
+echo llamamilk >> file.txt
+echo llama >> file.txt
+echo milk >> file.txt
+echo dirkachirpaldfk >> newfile.txt
+echo dirpaldfk >> newfile.txt
+echo daldfk >> newfile.txt
